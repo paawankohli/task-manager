09306bbcb98ea2632dbbb05c5326626055b8608a
Paawan Kohli <paawan.kohli10@gmail.com>
2020-05-21 16:07:15 +0530

    added title field in tasks

--- src/models/task.js
+++ src/models/task.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose")
 
 const TaskSchema = new mongoose.Schema({
  
+    title: {
+        type: String,
+        required: true,
+        default: "",
+        trim: true
+    },
+
     description: {
         type: String,
         required: true,
